d0b56b7794da233fb16aa9d96646ccc27cd7542e
anunay <anunay@arvindinternet.com>
2017-08-15 13:00:31 +0530

    Adding boilerplate code

---
+++ src/main/java/com/ail/learning/aerospikedemo/AerospikeDemoApplication.java
@@ -0,0 +1,12 @@
+package com.ail.learning.aerospikedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerospikeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AerospikeDemoApplication.class, args);
+	}
+}
